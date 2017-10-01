e2eb1123c2ddfb1396ab4292942fb47c1bc7bdbd
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-10-01 20:27:55 +0300

    Added live preview

--- resources/views/merch/unibutton.blade.php
+++ resources/views/merch/unibutton.blade.php
@@ -83,12 +83,37 @@
 <div class="panel-heading">
     <h3 class="panel-title">Button preview</h3>
   </div>
-<div class="panel-body">
-<button class="btn btn-primary center-block">Buy now with Ether</button>
+<div class="panel-body center-block" id="buttonPreview">
+<iframe frameborder="0"></iframe>
 </div>
 </div>
   <button type="submit" class="btn btn-default">Create button</button>
 </form>
 </div>
 </div></div></div>
+@endsection
+@section('add-js')
+<script>
+$(document).ready(function() {
+    $( ".form-control" ).change(function() {
+        var complexStyle = 'font-family: Helvetica, arial; font-weight: bold; border: none; text-align: center; text-decoration: none; display: inline-block; box-shadow: 0 4px 4px 0 rgba(0,0,0,0.2), 0 8px 10px 0 rgba(0,0,0,0.19); padding: 1em;';
+    var buttonText = $('#inputButtonText').val();
+
+    complexStyle += 'background-color: ' + $('#inputButtonColor').val() + '; color: white;';
+    if ($('#inputButtonSize').val()=='L') {
+        complexStyle += 'font-size: 20px; border-radius: 6px;';
+    } else if ($('#inputButtonSize').val()=='M') {
+        complexStyle += 'font-size: 16px; border-radius: 5px;';
+    } else {
+        complexStyle += 'font-size: 14px; border-radius: 3px;'
+    }
+
+    var frame = $('#buttonPreview iframe'),
+        body = frame.contents().find('body');
+
+    body.html('<div style="width: 100%; margin: auto;"><button style="'+complexStyle+'">'+buttonText+'</button></div');
+
+    });
+});
+</script>
 @endsection
