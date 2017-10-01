63e77ab983a200fc640ec6ba0f9606b3b1ef9bb5
Eugene Rupakov <eugene.rupakov@gmail.com>
2017-10-01 21:32:46 +0300

    Debug sending mail status

--- resources/views/vendor/laravel-user-verification/email-markdown.blade.php
+++ resources/views/vendor/laravel-user-verification/email-markdown.blade.php
@@ -1,10 +1,17 @@
 @component('mail::message')
 
-One last step!
+One last step left!
 
 @component('mail::button', ['url' => route('email-verification.check', $user->verification_token) . '?email=' . urlencode($user->email) ])
 Click here to verify your account
 @endcomponent
+
+## Please find your details below
+Merchant ID: $user->hash
+Merchant Secret: $user->secret
+Name: $user->firstName $user->familyName
+Project: $user->projectName
+Project URL: $user->projectURL
 
 Thanks,<br>
 {{ config('app.name') }}
